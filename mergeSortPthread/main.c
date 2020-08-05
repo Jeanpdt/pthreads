7a600a26c717548899f7e5e45c10da00f656e4dc
Jeanpdt <rodrigo.santospdt@gmail.com>
2020-08-05 13:30:51 -0400

    Mudanaça no função do mergesort com pthread

--- mergeSortPthread/main.c
+++ mergeSortPthread/main.c
@@ -1,203 +1,251 @@
-#include <pthread.h>
 #include <stdio.h>
+#include <pthread.h>
+#include <time.h>
 #include <stdlib.h>
 
-#define TYPE int
-#define MIN_LENGTH 4
+int opt_a;
+int opt_t;
+int opt_r;
 
-int DEBUG;
+int MAX;
+int THREAD_MAX;
+int *a;
 
-typedef struct {
-    TYPE *array;
-    int left;
-    int right;
-    int tid;
-} thread_data_t;
+struct tsk {
+    int tsk_no;
+    int tsk_low;
+    int tsk_high;
+};
 
-int number_of_threads;
-pthread_mutex_t lock_number_of_threads;
+void merge(int, int, int);
+void merge_sort(int, int);
+void *merge_sort123(void *);
 
-int my_comp(const void *, const void *);
-void merge(TYPE *, int, int, int);
-void *merge_sort_threaded(void *);
-void merge_sort(TYPE *, int, int);
+int main(int argc, char **argv)
+{
+    char *cp;
+    struct tsk *tsk;
+    FILE *fp;
+    int i, temp;
+    clock_t inicio, fim;
+    double time_taken;
 
-int main(int argc, char **argv) {
-    int n = 50000, temp, i;
+    --argc;
+    ++argv;
 
-    int *arr = (int *)malloc(n * sizeof(int));
+    MAX = 20000000;
+    THREAD_MAX = 2;
 
-    FILE * fp = fopen("ap.txt", "r");
+    // use new/general algorithm by default
+    opt_a = 1;
+
+    for (; argc > 0; --argc, ++argv) {
+        cp = *argv;
+        if (*cp != '-')
+            break;
+
+        switch (cp[1]) {
+        case 'M':  // array count
+            MAX = atoi(cp + 2);
+            break;
+
+        case 'T':  // thread count
+            THREAD_MAX = atoi(cp + 2);
+            break;
+
+        case 'a':  // change algorithm
+            opt_a = !opt_a;
+            break;
+
+        case 'r':  // do _not_ use rand -- use linear increment
+            opt_r = !opt_r;
+            break;
+
+        case 't':  // tracing
+            opt_t = !opt_t;
+            break;
+
+        default:
+            break;
+        }
+    }
+
+    // allocate the array
+    a = malloc(sizeof(int) * MAX);
+
+    fp = fopen("ag.txt", "r");
 
     i = 0;
     while (fscanf(fp, "%d", &temp) > 0)
     {
-        arr[i++] = temp;
+        a[i++] = temp;
     }
 
-    merge_sort(arr, 0, n - 1);
-    printf("After MergeSort.\n");
-	i = 0;
-    for (i = 0; i < n; ++i) {
-		printf("Num: %10d\n", arr[i]);
-	}
-    free(arr);
-    pthread_mutex_destroy(&lock_number_of_threads);
+    pthread_t threads[THREAD_MAX];
+    struct tsk tsklist[THREAD_MAX];
+
+    int len = MAX / THREAD_MAX;
+
+    if (opt_t)
+        printf("THREADS:%d MAX:%d LEN:%d\n", THREAD_MAX, MAX, len);
+
+    int low = 0;
+
+    for (int i = 0; i < THREAD_MAX; i++, low += len) {
+        tsk = &tsklist[i];
+        tsk->tsk_no = i;
+
+        if (opt_a) {
+            tsk->tsk_low = low;
+            tsk->tsk_high = low + len - 1;
+            if (i == (THREAD_MAX - 1))
+                tsk->tsk_high = MAX - 1;
+        }
+
+        else {
+            tsk->tsk_low = i * (MAX / THREAD_MAX);
+            tsk->tsk_high = (i + 1) * (MAX / THREAD_MAX) - 1;
+        }
+
+        if (opt_t)
+            printf("RANGE %d: %d %d\n", i, tsk->tsk_low, tsk->tsk_high);
+    }
+
+    inicio = clock();
+    // creating 4 threads
+    for (int i = 0; i < THREAD_MAX; i++) {
+        tsk = &tsklist[i];
+        pthread_create(&threads[i], NULL, merge_sort123, tsk);
+    }
+
+    // joining all 4 threads
+    for (int i = 0; i < THREAD_MAX; i++)
+        pthread_join(threads[i], NULL);
+
+    // show the array values for each thread
+    if (opt_t) {
+        for (int i = 0; i < THREAD_MAX; i++) {
+            tsk = &tsklist[i];
+            printf("SUB %d:", tsk->tsk_no);
+            for (int j = tsk->tsk_low; j <= tsk->tsk_high; ++j)
+                printf(" %d", a[j]);
+            printf("\n");
+        }
+    }
+
+    // merging the final 4 parts
+    if (opt_a) {
+        struct tsk *tskm = &tsklist[0];
+        for (int i = 1; i < THREAD_MAX; i++) {
+            struct tsk *tsk = &tsklist[i];
+            merge(tskm->tsk_low, tsk->tsk_low - 1, tsk->tsk_high);
+        }
+    }
+    else {
+        merge(0, (MAX / 2 - 1) / 2, MAX / 2 - 1);
+        merge(MAX / 2, MAX / 2 + (MAX - 1 - MAX / 2) / 2, MAX - 1);
+        merge(0, (MAX - 1) / 2, MAX - 1);
+    }
+    fim = clock();
+
+    // displaying sorted array
+    /*
+    printf("\n\nSorted array:");
+    for (int i = 0; i < MAX; i++)
+        printf(" %d", a[i]);
+    printf("\n");
+    */
+
+    time_taken = (double)(fim-inicio)/CLOCKS_PER_SEC; // in seconds
+    printf("mergeSort took %f seconds to execute \n", time_taken);
     return 0;
 }
 
-// Comparator function, just assumes that there is < and ==
-// operator on TYPE
-int my_comp(const void *larg, const void *rarg) {
-    TYPE l = *(TYPE *)larg;
-    TYPE r = *(TYPE *)rarg;
-    if (l < r) {
-        return -1;
-    } else if (l == r) {
-        return 0;
+
+// merge function for merging two parts
+void merge(int low, int mid, int high)
+{
+
+    // n1 is size of left part and n2 is size of right part
+    int n1 = mid - low + 1;
+    int n2 = high - mid;
+
+    int *left = malloc(n1 * sizeof(int));
+    int *right = malloc(n2 * sizeof(int));
+
+    int i;
+    int j;
+
+    // storing values in left part
+    for (i = 0; i < n1; i++)
+        left[i] = a[i + low];
+
+    // storing values in right part
+    for (i = 0; i < n2; i++)
+        right[i] = a[i + mid + 1];
+
+    int k = low;
+
+    i = j = 0;
+
+    // merge left and right in ascending order
+    while (i < n1 && j < n2) {
+        if (left[i] <= right[j])
+            a[k++] = left[i++];
+        else
+            a[k++] = right[j++];
     }
-    return 1;
+
+    // insert remaining values from left
+    while (i < n1)
+        a[k++] = left[i++];
+
+    // insert remaining values from right
+    while (j < n2)
+        a[k++] = right[j++];
+
+    free(left);
+    free(right);
 }
 
-void merge(TYPE *data, int left, int right, int tid) {
-    if (DEBUG) {
-        printf("[%d] Merging %d to %d\n", tid, left, right);
+// merge sort function
+void merge_sort(int low, int high)
+{
+
+    // calculating mid point of array
+    int mid = low + (high - low) / 2;
+
+    if (low < high) {
+        // calling first half
+        merge_sort(low, mid);
+
+        // calling second half
+        merge_sort(mid + 1, high);
+
+        // merging the two halves
+        merge(low, mid, high);
     }
-    int ctr = 0;
-    int i = left;
-    int mid = left + ((right - left) / 2);
-    int j = mid + 1;
-    int *c = (int *) malloc((right - left + 1) * sizeof(int));
-    while (i <= mid && j <= right) {
-        if (data[i] <= data[j]) {
-            c[ctr++] =  data[i++];
-        } else {
-            c[ctr++] =  data[j++];
-        }
-    }
-    // Either i = mid + 1 OR j = right + 1
-    if (i == mid + 1) {
-        while (j <= right) {
-            c[ctr++] = data[j++];
-        }
-    } else {
-        while (i <= mid) {
-            c[ctr++] = data[i++];
-        }
-    }
-    // Copy the data back !
-    i = left;
-    ctr = 0;
-    while (i <= right) {
-        data[i++] = c[ctr++];
-    }
-    free(c);
-    return;
 }
 
-// The function passed to a pthread_t variable.
-void *merge_sort_threaded(void *arg) {
-    thread_data_t *data = (thread_data_t *) arg;
-    int l = data->left;
-    int r = data->right;
-    int t = data->tid;
-    if (r - l + 1 <= MIN_LENGTH) {
-        // Length is too short, let us do a |qsort|.
-        if (DEBUG) {
-            printf("[%d] Calling qsort(%d, %d).\n", t, l, r);
-        }
-        qsort(data->array + l, r - l + 1, sizeof(TYPE), my_comp);
-    } else {
-        // Try to create two threads and assign them work.
-        int m = l + ((r - l) / 2);
-        // Data for thread 1
-        thread_data_t data_0;
-        data_0.left = l;
-        data_0.right = m;
-        data_0.array = data->array;
-        pthread_mutex_lock(&lock_number_of_threads);
-            data_0.tid = number_of_threads++;
-        pthread_mutex_unlock(&lock_number_of_threads);
-        // Create thread 1
-        pthread_t thread0;
-        int rc = pthread_create(&thread0,
-                                NULL,
-                                merge_sort_threaded,
-                                &data_0);
-        int created_thread_0 = 1;
-        if (rc) {
-            // Failed to create thread, call |qsort|.
-            if (DEBUG) {
-                printf("[%d] Failed to create thread, calling qsort.", data_0.tid);
-            }
-            created_thread_0 = 0;
-            qsort(data->array + l, m - l + 1, sizeof(TYPE), my_comp);
-        }
-        // Data for thread 2
-        thread_data_t data_1;
-        data_1.left = m + 1;
-        data_1.right = r;
-        data_1.array = data->array;
-        pthread_mutex_lock(&lock_number_of_threads);
-            data_1.tid = number_of_threads++;
-        pthread_mutex_unlock(&lock_number_of_threads);
-        // Create thread 2
-        pthread_t thread1;
-        rc = pthread_create(&thread1,
-                            NULL,
-                            merge_sort_threaded,
-                            &data_1);
-        int created_thread_1 = 1;
-        if (rc) {
-            // Failed to create thread, call |qsort|.
-            if (DEBUG) {
-                printf("[%d] Failed to create thread, calling qsort.", data_1.tid);
-            }
-            created_thread_1 = 0;
-            qsort(data->array + m + 1, r - m, sizeof(TYPE), my_comp);
-        }
-        // Wait for the created threads.
-        if (created_thread_0) {
-            pthread_join(thread0, NULL);
-        }
-        if (created_thread_1) {
-            pthread_join(thread1, NULL);
-        }
-        // Ok, both done, now merge.
-        // left - l, right - r
-        merge(data->array, l, r, t);
-    }
-    pthread_exit(NULL);
-    return NULL;
-}
+// thread function for multi-threading
+void * merge_sort123(void *arg)
+{
+    struct tsk *tsk = arg;
+    int low;
+    int high;
 
-void merge_sort(TYPE *array, int start, int finish) {
-    thread_data_t data;
-    data.array = array;
-    data.left = start;
-    data.right = finish;
-    // Initialize the shared data.
-    number_of_threads = 0;
-    pthread_mutex_init(&lock_number_of_threads, NULL);
-    data.tid = 0;
-    // Create and initialize the thread
-    pthread_t thread;
-    int rc = pthread_create(&thread,
-                            NULL,
-                            merge_sort_threaded,
-                            &data);
-    if (rc) {
-        if (DEBUG) {
-            printf("[%d] Failed to create thread, calling qsort.",
-                   data.tid);
-        }
-        qsort(array + start,
-              finish - start + 1,
-              sizeof(TYPE),
-              my_comp);
-    }
-    // Wait for thread, i.e. the full merge sort algo.
-    pthread_join(thread, NULL);
-    return;
-}
+    // calculating low and high
+    low = tsk->tsk_low;
+    high = tsk->tsk_high;
 
+    // evaluating mid point
+    int mid = low + (high - low) / 2;
+
+    if (low < high) {
+        merge_sort(low, mid);
+        merge_sort(mid + 1, high);
+        merge(low, mid, high);
+    }
+
+    return 0;
+}
